0a03ab4f52b41f091d572ccb358a7d65e03b61ae
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-07 19:15:51 -0300

    backend(feat): added method that returns fruits below the minimum stock

--- backend/infra/repositories/fruit_repository.py
+++ backend/infra/repositories/fruit_repository.py
@@ -16,3 +16,8 @@ class FruitRepository:
         return self.session.exec(
             select(Fruit).order_by(Fruit.quantity.desc())
         ).first()
+    
+    def fruits_below_min_stock(self) -> List[Fruit]:
+        return self.session.exec(
+            select(Fruit).where(Fruit.quantity < Fruit.min_stock)
+        ).all()
