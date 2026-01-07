badb1f89aa977370e4660e6b1bef000c455c935d
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-07 19:27:05 -0300

    backend(feat): added method to count fruits in the service

--- backend/domain/services/fruit_service.py
+++ backend/domain/services/fruit_service.py
@@ -4,3 +4,7 @@ class FruitService:
 
     def __init__(self, repository: FruitRepository):
         self.repository = repository
+    
+    def count_fruits(self) -> int:
+        """Retorna o número total de frutas cadastradas"""
+        return self.repository.count_fruits()
