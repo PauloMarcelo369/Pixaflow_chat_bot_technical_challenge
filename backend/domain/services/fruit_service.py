0fd5ceebfe7eb1623b4e391a9625eb4f8ae9ca6e
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-07 19:35:24 -0300

    backend(feat): added a method that returns the fruit with the highest quantity in the service

--- backend/domain/services/fruit_service.py
+++ backend/domain/services/fruit_service.py
@@ -8,3 +8,10 @@ class FruitService:
     def count_fruits(self) -> int:
         """Retorna o número total de frutas cadastradas"""
         return self.repository.count_fruits()
+    
+    def get_fruit_with_max_quantity(self) -> str:
+        """Retorna a fruta com maior quantidade em formato legível"""
+        fruit = self.repository.fruit_with_max_quantity()
+        if not fruit:
+            return "Nenhuma fruta encontrada."
+        return f"{fruit.name} tem a maior quantidade ({fruit.quantity} {fruit.unit})"
