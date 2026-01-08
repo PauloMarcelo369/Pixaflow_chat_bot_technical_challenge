d08a6a6ec94786190a664e60ba9ff3ebe0ecf7f8
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-07 21:29:06 -0300

    backend(feat_: added Fruit intention Handler

---
+++ backend/domain/intents/fruit_intent_handler.py
@@ -0,0 +1,16 @@
+from domain.intents.fruit_intent import FruitIntent
+from domain.services.fruit_service import FruitService
+
+class FruitIntentHandler:
+    def __init__(self, service: FruitService): 
+        self.service = service
+    def handle(self, intent: FruitIntent, params: dict) -> str: 
+        if intent == FruitIntent.COUNT_FRUITS: 
+            return str(self.service.count_fruits()) 
+        if intent == FruitIntent.FRUIT_WITH_MAX_QUANTITY: 
+            return self.service.get_fruit_with_max_quantity() 
+        if intent == FruitIntent.FRUITS_BELOW_MIN_STOCK: 
+            return self.service.get_fruits_below_min_stock() 
+        if intent == FruitIntent.GET_FRUIT_INFO: 
+            return self.service.get_fruit_info(params.get("name")) 
+        raise ValueError("Intent não suportada")
